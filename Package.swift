132cddd2c97a8c42c4064e58fc5db825f8c3026e
Paul Schifferer <paul@schifferers.net>
2026-01-25 14:55:01 -0800

    Update version

--- Package.swift
+++ Package.swift
@@ -1,13 +1,16 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.2
+//
+// DriveThruRPG SDK
+// Copyright (c) 2026 Pilgrimage Softwware
+// Licensed under the MIT License
+//
 
 import PackageDescription
 
 let package = Package(
     name: "DriveThruRPGSDK",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.6.0"),
-        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.7.0"),
+platforms: [
+        .macOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -16,21 +19,32 @@ let package = Package(
             targets: ["DriveThruRPG"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.10.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.9.0"),
+                .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
+
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "DriveThruRPG",
-        ),
-        .testTarget(
-            name: "DriveThruRPGTests",
-            dependencies: ["DriveThruRPG"]
-        ),
-    ],
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+            ],
             swiftSettings: swiftSettings,
             plugins: [
                 .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
             ]
+        ),
+        .testTarget(
+            name: "DriveThruRPGTests",
+            dependencies: ["DriveThruRPG"],
+            swiftSettings: swiftSettings
+        ),
+    ]
 )
 
 var swiftSettings: [SwiftSetting] {
