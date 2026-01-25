ae3b87afc2a57fc1988a97426cfd1b749b74f2a7
Paul Schifferer <paul@schifferers.net>
2026-01-25 13:36:33 -0800

    Build bits

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DriveThruRPGSDK",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.7.0"),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DriveThruRPG",
+            targets: ["DriveThruRPG"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DriveThruRPG",
+        ),
+        .testTarget(
+            name: "DriveThruRPGTests",
+            dependencies: ["DriveThruRPG"]
+        ),
+    ],
+            swiftSettings: swiftSettings,
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
+            ]
+)
+
+var swiftSettings: [SwiftSetting] {
+    [
+        .enableUpcomingFeature("ExistentialAny"),
+        .swiftLanguageMode(.v5),
+    ]
+}
